b3c350b5e92c2a7497e166de05df2355a7011e5c
PD <praveendaspd@gmail.com>
2021-06-05 12:42:52 +1000

    Controller Test Case added
    TDD approach, started with a failing test case and built my way up to get a passing controller test as I fleshed out the controller , service and domain classes

--- src/main/java/com/vanguard/coding/challenge/weather/rest/controller/WeatherController.java
+++ src/main/java/com/vanguard/coding/challenge/weather/rest/controller/WeatherController.java
@@ -3,10 +3,33 @@
  */
 package com.vanguard.coding.challenge.weather.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vanguard.coding.challenge.weather.rest.domain.Weather;
+import com.vanguard.coding.challenge.weather.rest.service.WeatherService;
+
 /**
  * @author praveendas
  *
  */
+@RestController
 public class WeatherController {
+	
+	@Autowired
+	WeatherService weatherService;
+	
+	@GetMapping("/getWeather")
+	public ResponseEntity<Weather> getCurrentWeather(){
+		
+		Weather weather = new Weather();
+		
+		weather = weatherService.getWeatherDetails();
+		
+		return new ResponseEntity<Weather>(weather,HttpStatus.OK);
+	}
 
 }
