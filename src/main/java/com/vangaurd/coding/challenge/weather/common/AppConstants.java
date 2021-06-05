8200d9e5474274e96678454459bc7006c1820490
PD <praveendaspd@gmail.com>
2021-06-05 16:07:01 +1000

    Added logger for WeatherApplication

---
+++ src/main/java/com/vangaurd/coding/challenge/weather/common/AppConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.vangaurd.coding.challenge.weather.common;
+
+/**
+ * @author praveendas
+ *
+ */
+public class AppConstants {
+	
+	public static String NOTIFY_ADMIN = "NOTIFY_ADMIN";
+
+}
