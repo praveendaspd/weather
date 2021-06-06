f47db9ba63448d40c9569c6c66d4a3f7033c8be1
PD <praveendaspd@gmail.com>
2021-06-06 18:00:05 +1000

    Added Exception Handling

---
+++ src/main/java/com/vanguard/coding/challenge/weather/rest/exception/InternalServerException.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.vanguard.coding.challenge.weather.rest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author praveendas
+ *
+ * Thrown when the server encountered an unexpected condition that prevented it from fulfilling the
+ * request
+ */
+@ResponseStatus(value =HttpStatus.INTERNAL_SERVER_ERROR, reason = "Internal Server Error!")
+public class InternalServerException extends BaseException {
+
+	private static final long serialVersionUID = 1L;
+
+
+	public InternalServerException() {
+        super();
+    }
+
+    public InternalServerException(String message) {
+        super(message);
+    }
+
+
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public InternalServerException(Throwable cause) {
+        super(cause);
+    }
+
+}
+
